0881363ff5c33ab75dfbe94c3229e79da20e8772
archisha.m <architen100@gmail.com>
2019-04-22 20:19:46 -0500

    Added to my development file

---
+++ MealGenerator/src/Tests/ParserTest.cpp
@@ -0,0 +1,17 @@
+//
+//  ParserTest.cpp
+//  MealGenerator
+//
+//  Created by Archisha Majee on 4/22/19.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <stdio.h>
+#include "ofApp.h"
+#include "/Users/archi/Desktop/catch3.hpp"
+
+TEST_CASE("test for size of recipes") {
+//    ofApp *app = new ofApp();
+//    std::vector<Recipes> recipes = app->Parser();
+    REQUIRE(19 == 19);
+}
